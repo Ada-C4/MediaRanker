96185c7d2d54076cd52fc7e4704051556088e051
noglows <jessica.noglows@outlook.com>
2015-11-30 13:45:14 -0800

    /movies page functional

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,11 @@ class MoviesController < ApplicationController
   def all_media
     @movies = Movie.all
     @books = Book.all
-    @albums = Album.all 
+    @albums = Album.all
+  end
+
+  def index
+    @movies = Movie.all
   end
 
 end
