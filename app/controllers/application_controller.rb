17ac202542afde69279650eb09ffefc5a4daf31a
Ricky Hougland <houglande@gmail.com>
2015-11-30 12:31:06 -0800

    add contents to root view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def sort(data)
+    d = data.sort_by { |instance| instance.ranking }
+    d.reverse
+  end
+
+  def index
+    @movies = sort(Movie.all)
+    @books = sort(Book.all)
+    @albums = sort(Album.all)
+  end
 end
