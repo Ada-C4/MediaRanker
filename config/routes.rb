ab36e37fbdfc34ff33014fa25cf48a2cd95a20a1
Tammy <misstamar@gmail.com>
2015-11-30 14:03:19 -0800

    made home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  root to: 'welcome#index'
+  
+  resources :books do
+  end
+
+  resources :albums do
+
+  end
+
+  resources :movies do
+
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
