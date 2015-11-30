4b81b42b37711807b8c96fd6e9a8f3cb7d346a7f
Desiree <desireepoland@gmail.com>
2015-11-30 13:54:33 -0800

    Generate book model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130214422) do
+ActiveRecord::Schema.define(version: 20151130215036) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name"
+    t.string   "author"
+    t.string   "description"
+    t.integer  "upvotes",     default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
