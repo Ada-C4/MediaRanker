2920d1034cad6a3703f8caf3ec19552bba95579d
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-30 14:53:35 -0800

    Skeleton of home page complete. Needs CSS for sure.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
   def all_media
   	# x is the number of items that will show in the 'top' display
   	x = 2
-  	@top_movies = Movie.order(:votes).limit(x)
+  	@top_movies = Movie.order(votes: :desc).limit(x)
   	@top_books = Book.order(votes: :desc).limit(x)
-  	@top_albums = Album.order(:votes).limit(x)
+  	@top_albums = Album.order(votes: :desc).limit(x)
   end
 
 end
