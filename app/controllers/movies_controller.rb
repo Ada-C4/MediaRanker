b4c155ffb717f861a5cd169d28d9ba9cd9ef2ae6
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-30 13:40:11 -0800

    index and new actions/view for Movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,25 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+    # code to sort movies by number of votes
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params[:movie])
+    @movie.save
+
+    redirect_to movies_path
+  end
+
+
+  private
+
+  def movie_params
+    params.permit(movie:[:title, :director, :description])
+  end
+
 end
