5ef28c9983b9fcf5e37fed7b036029711ab5bde6
Dreedle <davis.audrey.lynn@gmail.com>
2015-11-30 14:26:13 -0800

    On home index, showing all books, albums, and movies (not limited to top ten yet). Did put in some bootstrap classes in anticipation of the future

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
+  before_action :get_movies
+  before_action :get_books
+  before_action :get_albums
+
+  def get_movies
+    @movies = Movie.all
+  end
+
+  def get_books
+    @books = Book.all
+  end
+
+  def get_albums
+    @albums = Album.all
+  end
+
   def index
   end
 end
