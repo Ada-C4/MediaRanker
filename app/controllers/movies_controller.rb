0922379df800d40003b0e14e8ac6c23e2a8f9dc5
Ricky Hougland <houglande@gmail.com>
2015-11-30 13:41:04 -0800

    add index for movies with table showing all movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,23 @@
 class MoviesController < ApplicationController
+
+  def sort(data)
+    d = data.sort_by { |instance| instance.ranking }
+    d.reverse
+  end
+
+  def index
+    @movies = sort(Movie.all)
+  end
+
+  def show
+
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+
+  end
 end
