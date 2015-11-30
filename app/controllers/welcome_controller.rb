01450e536c32c53c3dec68ffe3ca5d9957bbe4cc
Kelly <kelly_devlin@alumni.brown.edu>
2015-11-30 15:54:56 -0800

    Create root route with list of 10 from each table

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  def index
+    @movies = Movie.all.limit(10)
+    @books = Book.all.limit(10)
+    @albums = Album.all.limit(10)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
   patch '/movies/:id/upvote' => 'movies#upvote', as: :movie_upvote
   patch '/books/:id/upvote' => 'books#upvote', as: :book_upvote
   patch '/albums/:id/upvote' => 'albums#upvote', as: :album_upvote
