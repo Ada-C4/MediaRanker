ef33434422d7838b567b7761b18eb290faa7b4be
Brittany <BrittanyKohler@gmail.com>
2015-11-30 12:28:11 -0800

    Updating CRUD methods for movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,55 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.where(:user_id => params[:user_id])
+  end
+
+  def show
+    id = params[:id]
+    @movie = Movie.find(id)
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    Movie.create(movie_params[:movie])
+    redirect_to movies_path
+  end
+
+  def destroy
+    id = params[:id]
+    Movie.delete(id)
+    redirect_to movies_path
+  end
+
+  def edit
+    id = params[:id]
+    @movie = Movie.find(id)
+    session[:return_to] = request.referer
+  end
+
+  def update
+    id = params[:id]
+    movie = Movie.find(id)
+    movie.update(
+    name: movie_params[:movie][:name],
+    director: movie_params[:movie][:name],
+    description: movie_params[:movie][:description],
+    )
+    movie.save
+    if session[:return_to].nil?
+      redirect_to "/"
+    else
+      redirect_to session[:return_to]
+    end
+    session[:return_to] = nil
+  end
+
+
+  private
+
+  def movie_params
+    params.permit(movie:[:name, :director, :description])
+  end
 end
