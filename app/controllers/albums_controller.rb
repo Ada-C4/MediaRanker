0965ca2df7b4e2b036c670d1190bedc31a20e720
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-11-30 15:10:24 -0800

    added new/edit for albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -8,5 +8,35 @@ class AlbumsController < ApplicationController
     @album = Album.find(id)
   end
 
+  def new
+    @album = Album.new
+  end
+
+  def create
+    id = params[:id]
+    Album.create(album_params[:album])
+    redirect_to "/albums/#{id}"
+  end
+
+  def edit
+    id = params[:id]
+    @album = Album.find(id)
+  end
+
+  def update
+    id = params[:id]
+    @album = Album.find(id)
+    @album.update(
+    name: album_params[:album][:name],
+    artist: album_params[:album][:artist],
+    description: album_params[:album][:description]
+    )
+    redirect_to "/albums/#{id}"
+  end
+
   private
+
+  def album_params
+    params.permit(album:[:name, :artist, :description, :upvotes])
+  end
 end
