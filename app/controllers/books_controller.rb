1daa364688b280711d28845fa84b188bb2ff0833
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-11-30 15:06:26 -0800

    added new/edit for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,5 +8,35 @@ class BooksController < ApplicationController
     @book = Book.find(id)
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    id = params[:id]
+    Book.create(book_params[:book])
+    redirect_to "/books/#{id}"
+  end
+
+  def edit
+    id = params[:id]
+    @book = Book.find(id)
+  end
+
+  def update
+    id = params[:id]
+    @book = Book.find(id)
+    @book.update(
+    name: book_params[:book][:name],
+    author: book_params[:book][:author],
+    description: book_params[:book][:description]
+    )
+    redirect_to "/books/#{id}"
+  end
+
   private
+
+  def book_params
+    params.permit(book:[:name, :author, :description, :upvotes])
+  end
 end
