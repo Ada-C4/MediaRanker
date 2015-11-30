1a6d66254e644e8131e960327bb67aad372c6115
Dreedle <davis.audrey.lynn@gmail.com>
2015-11-30 15:36:12 -0800

    Upvote works for show on all three mvcs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   post 'movies/:id/upvote' => 'movies#upvote', as: :movie_upvote
+  post 'books/:id/upvote' => 'books#upvote', as: :book_upvote
+  post 'albums/:id/upvote' => 'albums#upvote', as: :album_upvote
   resources :movies
   resources :books
   resources :albums
