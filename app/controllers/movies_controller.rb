e2fdfc3b3d096f76a2fda9d6a16c7ba915efb56a
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-30 14:18:25 -0800

    added CRUD actions to movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,22 +1,47 @@
 class MoviesController < ApplicationController
+	before_action :set_movie, only: [:edit, :show, :update, :destroy] 
+
   def new
+  	@movie = Movie.new
   end
 
   def create
-  end
-
-  def destroy
+  	@movie = Movie.new(movie_params)
+  	if @movie.save
+  		redirect_to movie_path(@movie)
+  	else
+  		render 'new'
+  	end
   end
 
   def edit
   end
 
   def update
+  	@movie.update(movie_params)
+  	redirect_to movie_path(@movie)
   end
 
   def index
+  	@movies = Movie.all
   end
 
   def show
   end
+
+  def destroy
+  	@movie.destroy
+  	redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+  	return params.require(:movie).permit(:name, :director, :description)
+  end
+
+  def set_movie
+  	@movie = Movie.find(params[:id])
+  end
+
 end
