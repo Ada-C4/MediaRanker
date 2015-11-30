e41d6c1561770ae646185a26ff4fc5d2f46a6daf
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-30 14:22:20 -0800

    CRUD actions added to albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,13 +1,46 @@
 class AlbumsController < ApplicationController
+	before_action :set_album, only: [:edit, :show, :update, :destroy] 
+
   def new
+  	@album = Album.new
+  end
+
+  def create
+  	@album = Album.new(album_params)
+  	if @album.save
+  		redirect_to album_path(@album)
+  	else 
+  		render 'new'
+  	end
   end
 
   def edit
   end
 
+  def update
+  	@album.update(album_params)
+  	redirect_to album_path(@album)
+  end
+
   def index
+  	@albums = Album.all
   end
 
   def show
+  end
+
+  def destroy
+  	@album.destroy
+  	redirect_to albums_path
+  end
+
+  private 
+
+  def album_params
+  	
+  end
+
+  def set_album
+  	@album = Album.find(params[:id])
   end
 end
