df58496d7e077e51ea21c6e32bfdde4044a84129
Amy Hunter <amyrhunter@gmail.com>
2015-11-30 14:34:41 -0800

    Create movie model and controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,43 @@
+class MoviesController < ApplicationController
+  class CompaniesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @url = "create"
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params[:movie])
+    redirect_to "/movies"
+  end
+
+  def show
+    @movie = Movie.find(params[:movie])
+  end
+
+  def edit
+    @url = "update"
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    Movie.update(params[:id], movie_params[:movie])
+    redirect_to "/movies"
+  end
+
+  def destroy
+    Company.destroy(params[:id])
+    redirect_to "/movies/"
+  end
+
+private
+
+  def movie_params
+    params.permit(movie: [:name, :director, :description, :rank])
+  end
+end
+
+end
