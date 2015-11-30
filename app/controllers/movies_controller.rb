ce25711e576d3d22fc6f2832c6c8330d09b833e5
Claire <ClaireS29@gmail.com>
2015-11-30 12:25:35 -0800

    Created movies model and controller with restful routes

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,21 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def delete
+
+  end
+end
