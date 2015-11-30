1db3e8d683354ef08bfcf186a0fff59012e72a44
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-30 14:37:57 -0800

    seeds written

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+movies = [
+	{name: 'Harold and Maude', description: 'The best', director: 'No idea'}, 
+	{name: 'A movie', description: "it's cool", director: "mr. cool guy" }, 
+	{name: "another movie", description: "fdsf", director: "fdsfds"}, 
+	{name: "the never ending story" , description: "atreyu", director: "who knows"}, 
+	{name: "spice world", description: "the only movie you need", director: "probably a white dude"}, 
+]
+
+books = [
+	{name: "vile bodies", author: "evelyn waugh", description: "this is something about a book"}, 
+	{name: "war and peace", author: "tolstoy", description: "this is something about a book"}, 
+	{name: "clifford the big red dog" , author: "dunno", description: "this is something about a book"}, 
+	{name: "the dictionary", author: "everyone", description: "all teh wordz"}, 
+	{name: "A book", author: "probably a dude", description: "this is something about a book"}, 
+]
+
+albums = [
+	{name: "Eminem Show", artist: "Eminem", description: "middle school anthems" }, 
+	{name: "Some Music", artist:"Some Artist", description: "This is a thing about music"}, 
+	{name: "musica", artist: "artista", description: "i know i'm missing pronouns"}, 
+	{name: "jdslkfksd", artist: "artist formerly known as prince", description: "a thing"}, 
+	{name:"now 1", artist: "everyone", description: "now THAT'S what I call music"}, 
+]
+
+
+
+movies.each do |movie|
+	Movie.create(movie)
+end
+
+albums.each do |album|
+	Album.create(album)
+end
+
+books.each do |book|
+	Book.create(book)
+end
