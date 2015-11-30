a98198e736dd1288d60964aa4b7e786c0119856b
emgord <emgord@gmail.com>
2015-11-30 14:35:55 -0800

    added new and create method for movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,21 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    new_movie = Movie.create(movie_params[:movie])
+    redirect_to movie_path(new_movie)
+  end
+
+  private
+
+  def movie_params
+    params.permit(movie:[:name, :director, :description, :rank])
+  end
+
+
 end
