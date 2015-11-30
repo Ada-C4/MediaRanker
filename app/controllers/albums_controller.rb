ef951c51eed78a89c650d07bb0a5e9d10a65f8d8
Ricky Hougland <houglande@gmail.com>
2015-11-30 14:34:44 -0800

    copy and pasting controller methods from movies to books and albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,60 @@
 class AlbumsController < ApplicationController
+  def sort(data)
+    d = data.sort_by { |instance| instance.ranking }
+    d.reverse
+  end
+
+  def index
+    @albums = sort(Album.all)
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(strong_params)
+    if @album.save
+      redirect_to album_path(@album.id)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+    @album.attributes = strong_params
+    if @album.save
+      render "show"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    album = Album.find(params[:id])
+    album.destroy
+    redirect_to albums_path
+  end
+
+  def upvote
+    @album = Album.find(params[:id])
+    @album.ranking += 1
+    @album.save
+    render "show"
+  end
+
+  private
+
+  def strong_params
+    params.require(:album).permit(:name, :director, :description, :ranking)
+  end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,10 +19,37 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(strong_params)
     if @book.save
-      redirect_to movie_path(@book.id)
+      redirect_to book_path(@book.id)
     else
       render "new"
     end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.attributes = strong_params
+    if @book.save
+      render "show"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def upvote
+    @book = Book.find(params[:id])
+    @book.ranking += 1
+    @book.save
+    render "show"
   end
 
   private
