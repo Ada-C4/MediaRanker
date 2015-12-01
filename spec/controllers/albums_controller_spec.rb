31ef4ec6493f38c8206e6afaaa54c261a82c590e
Brittany <BrittanyKohler@gmail.com>
2015-12-01 13:53:05 -0800

    Adding some tests for albums controller

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe AlbumsController, type: :controller do
+  describe "GET 'index'" do
+      it "is successful" do
+        get :index
+        expect(response.status).to eq 200
+      end
+    end
 
+    describe "GET 'new'" do
+      it "renders new view" do
+        get :new
+        expect(subject).to render_template :new
+      end
+    end
+
+    describe "GET 'show'" do
+      let(:album) do
+        Album.create(name: "Test")
+      end
+
+      it "renders the show view" do
+        get :show, id: album.id
+        expect(subject).to render_template :show
+      end
+    end
+
+    describe "POST 'create'" do
+      let(:good_params) do
+      {
+        album: {
+          name: "Something something something"
+        }
+      }
+      end
+
+      let (:bad_params) do
+        {
+          album: {}
+        }
+      end
+
+      it "redirects to index page" do
+        subject { post :create, good_params }
+
+        # Same as above
+        # post :create, params
+
+        # Success case to index page
+        expect(subject).to redirect_to albums_path
+      end
+
+      it "renders new template on error" do
+        # Error case to show errors on form
+        post :create, bad_params
+        expect(subject).to render_template :new
+      end
+    end
 end
