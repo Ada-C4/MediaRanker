f936a2e3d5e61424de4d790267b6edb1ee5c1d7f
Claire <ClaireS29@gmail.com>
2015-12-01 15:37:55 -0800

    Added index partial and controller methods for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,50 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @books = Book.find(params[:id])
+  end
+
+  def new
+    @books = Book.new
+  end
+
+  def create
+    @books = Book.create(book_params[:book])
+    if @books.save
+      redirect_to book_path(@books.id)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @books = Book.find(params[:id])
+  end
+
+  def update
+    @books = Book.find(params[:id])
+    book.update(params[:id], book_params[:book])
+    redirect_to book_path(params[:id])
+  end
+
+  def destroy
+    Book.destroy(params[:id])
+    redirect_to books_path
+  end
+
+  def upvote
+    @books = Book.find(params[:id])
+    @books.rank += 1
+    @books.save
+    redirect_to book_path(@books.id)
+  end
+
+  private
+
+  def book_params
+    params.permit(movie: [:name, :author, :description])
+  end
 end
