f0dfb7eabe2f2c83462999fcb1cb463328c542e2
Dreedle <davis.audrey.lynn@gmail.com>
2015-12-01 11:31:20 -0800

    Adding some spec tests for album models

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".validates" do
+    it "must have a title" do
+      expect(Album.new(title: nil)).to_not be_valid
+    end
+
+    it "must have a description" do
+      expect(Album.new(description: nil)).to be_invalid
+    end
+
+    it "must have an artist" do
+      expect(Album.new(artist: nil)).to_not be_valid
+    end
+
+    it "must have a ranking" do
+      expect(Album.new(ranking: nil)).to_not be_valid
+    end
+
+    it "must have a ranking of zero on creation" do
+      expect(Album.new(ranking: 1)).to_not be_valid
+      expect(Album.new(ranking: 0)).to be_valid
+    end
+  end
 end
