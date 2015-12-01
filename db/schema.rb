6f369e8381e7e5f5b3b8b810af96f58617163d3f
Claire <ClaireS29@gmail.com>
2015-11-30 22:48:06 -0800

    added album and book model and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130202136) do
+ActiveRecord::Schema.define(version: 20151201064550) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "name"
+    t.string   "artist"
+    t.string   "description"
+    t.integer  "rank"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name"
+    t.string   "author"
+    t.string   "description"
+    t.integer  "rank"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
