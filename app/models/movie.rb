c3d3524f28eea03d89f1b5fbaa9f92282f6fbdbe
Meighan Rasley <meighanr@gmail.com>
2015-12-01 14:07:57 -0800

    Add validations to movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ActiveRecord::Base
   validates :name, presence: true
-
+  validates :director, presence: true
+  validates :description, presence: true
+  validates :rank, presence: true, numericality: true
 end
