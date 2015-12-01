2271b5f22b663a082e9a688a90d41c1793c5c9c3
emgord <emgord@gmail.com>
2015-12-01 14:02:53 -0800

    added simplecov

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
-  describe "GET 'index'" do
-    it "is successful" do
-      get :index
-      expect(response.status).to eq 200
+    describe "GET 'index'" do
+      it "is successful" do
+        get :index
+        expect(response.status).to eq 200
+      end
+    end
+
+    describe "GET 'new'" do
+      it "renders new view" do
+        get :new
+        expect(subject).to render_template :new
     end
   end
-
 end
