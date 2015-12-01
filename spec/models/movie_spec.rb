db8415bf0378a067d850dfa94db3e0f44eff425a
Claire <ClaireS29@gmail.com>
2015-12-01 11:13:58 -0800

    Added initial movie specs

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Movie, type: :model do
+  describe ".validates" do
+    it "must have a name" do
+      expect(Movie.new(name: nil)).to_not be_valid
+    end
+  end
+end
