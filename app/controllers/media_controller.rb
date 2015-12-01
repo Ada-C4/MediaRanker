28ceb15abe482bda92bd90b595df2933b066d15f
Desiree <desireepoland@gmail.com>
2015-11-30 16:44:23 -0800

    Fill out more on home page, order media by their upvote amounts

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,4 +1,7 @@
 class MediaController < ApplicationController
   def index
+    @books = Book.all.order(:upvotes).reverse.take(5)
+    @albums = Album.all.order(:upvotes).reverse.take(5)
+    @movies = Movie.all.order(:upvotes).reverse.take(5)
   end
 end
