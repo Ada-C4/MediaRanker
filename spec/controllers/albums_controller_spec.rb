f8228217d878f8adfe49c12b3c3a5f0a1de7d28a
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-01 13:53:05 -0800

    basic specs added for albums

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -11,6 +11,48 @@ RSpec.describe AlbumsController, type: :controller do
 	    end
 
 	  end
-	end
 
+	  describe "GET 'show'" do
+	  	let(:album) do
+	  		Album.create(name: 'a album')
+	  	end
+	  	it "renders the show view" do
+	  		get :show, id: album.id
+	  		expect(response.status).to eq 200
+	  	end
+	  end
+
+    describe "POST 'create'" do
+    	let(:params) do
+    		{
+    			album: 
+    			{
+    				name: "this is a name"
+    			}
+    		}
+    	end
+
+    	let(:bad_params) do
+    		{
+    			album:
+    			{
+    				description: 'this'
+    			}
+    		}
+    	end
+
+    	# item created properly
+    	it "redirects to index page" do
+    		post :create, params
+    		expect(subject).to redirect_to album_path(1)
+    	end
+
+    	# error in creation
+    	it "renders new template" do
+    		post :create, bad_params
+    		expect(subject).to render_template("new")
+    	end
+    end
+
+	end
 end
