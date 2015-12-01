86903101bdba1536fb5d42416671bd2ed9bbde74
Daphne <dg1181@nyu.edu>
2015-11-30 17:21:34 -0800

    Can now add a new item or show an item

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,52 @@
 class MoviesController < ApplicationController
+  def index
+    @items = Movie.order(votes: :desc)
+    @type = "Movie"
+
+    render "shared/index"
+  end
+
+  def show
+    @item = Movie.find(params[:id])
+    @type = "Movies"
+    @special = "Directed"
+
+
+    render "shared/show"
+  end
+
+  def new
+    @item = Movie.new
+    @special = "Director"
+
+    render "shared/new"
+  end
+
+  def create
+    @item = Movie.new(movie_params)
+
+    if @item.save
+      redirect_to movies_path
+    else
+      render "shared/new"
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :creator, :description)
+  end
 end
