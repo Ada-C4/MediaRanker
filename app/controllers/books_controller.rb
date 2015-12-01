da7e1e15a390d172c2e069f28def85d6de6f58ee
Tammy <misstamar@gmail.com>
2015-12-01 07:57:23 -0800

    completed views and controller for movies (exapt delete)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,19 @@
 class BooksController < ApplicationController
 
   def get_book
-    @book = Book.find(params[:book_id])
+    @book = Book.find(params[:id])
   end
 
   def index
+    @books = Book.all
   end
 
   def new
+    @album = Album.new
+  end
+
+  def edit
+    get_book
   end
 
   def show
@@ -15,6 +21,16 @@ class BooksController < ApplicationController
   end
 
   def update
+    if params[:class] == "upvote"
+      get_book
+      rank = @book[:rank] + 1
+      @book.update(:rank => rank)
+      render "show"
+    else
+      get_book
+      @book.update(book_params[:book])
+      redirect_to book_path
+    end
   end
 
   def create
@@ -26,7 +42,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.permit(:book[:name, :author, :description])
+    params.permit(book: [:name, :author, :description, :rank])
   end
 
 end
