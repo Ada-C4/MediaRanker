bf9991ac3e530e61d5fc48344044017835bde10c
Desiree <desireepoland@gmail.com>
2015-12-01 14:48:04 -0800

    Add specs for show view for movies and albums controllers

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe AlbumsController, type: :controller do
       expect(response.status).to eq 200
     end
   end
+
+  describe "GET 'show/:id'" do
+    let(:album) do
+      Album.create(name: "Some Title")
+    end
+
+    it "renders show view" do
+      get :show, id: album.id
+      expect(response.status).to eq 200
+    end
+  end
 end
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe MoviesController, type: :controller do
       expect(response.status).to eq 200
     end
   end
+
+  describe "GET 'show/:id'" do
+    let(:movie) do
+      Movie.create(name: "Some Title")
+    end
+
+    it "renders show view" do
+      get :show, id: movie.id
+      expect(response.status).to eq 200
+    end
+  end
 end
