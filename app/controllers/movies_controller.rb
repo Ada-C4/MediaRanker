5efb39894e488d8d267199530a43ba55bbd4e110
Desiree <desireepoland@gmail.com>
2015-12-01 14:04:19 -0800

    Add index view for movies and upvote method and route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
  def index
-
+   @movies = Movie.all.order(:upvotes).reverse
  end
 
  def show
@@ -27,5 +27,18 @@ class MoviesController < ApplicationController
  def destroy
 
  end
+
+ def upvote
+   movie = Movie.find(params[:id])
+   movie.upvotes += 1
+   movie.save!
+   redirect_to movie_path(params[:id])
+ end
+
+ private
+
+  def movie_params
+    params.require(:movie).permit(:name, :director, :description)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :albums
 
   post "/books/:id"  => "books#upvote",  as: :upvote_book
+  post "/movies/:id"  => "movies#upvote",  as: :upvote_movie
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
