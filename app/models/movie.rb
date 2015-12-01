d82dbaae5a75f1f30e1cc8b3f3af095266f14c89
Dreedle <davis.audrey.lynn@gmail.com>
2015-12-01 14:36:14 -0800

    Adding validations for ranking to book and movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ActiveRecord::Base
   validates :title, presence: true
+  validates :ranking, presence: true, numericality: {equal_to: 0}, on: :create
   validates :ranking, presence: true
+
 
   def self.top_ten
     Movie.order("ranking DESC").limit(10)
