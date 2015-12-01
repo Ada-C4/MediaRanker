836d0af0dd31125dfe3719ac6b76cfe39e928472
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-01 11:08:31 -0800

    Setup rspec and manually create files

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  # describe ".validates" do
+  #   it "must have a body" do
+  #     expect(Post.new(body: nil)).to_not be_valid
+  #   end
+  #
+  #   it "can't have 141 characters" do
+  #     expect(Post.new(body: "a" * 141)).to be_invalid
+  #   end
+  #
+  #   it "can have 140 characters" do
+  #     expect(Post.new(body: "a" * 140)).to be_valid
+  #   end
+  # end
+  #
+  # it "counts its characters" do
+  #   expect(Post.new(body: "b" * 122).character_count).to eq 122
+  # end
+end
