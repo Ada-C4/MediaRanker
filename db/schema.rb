f38f9d1b13f2b2ca6441fe97428eb6930e0f632b
Amy Hunter <amyrhunter@gmail.com>
2015-12-01 14:14:20 -0800

    Add non working link to an all media page that has yet to be created and made a book model and seed data base for book model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130222600) do
+ActiveRecord::Schema.define(version: 20151201220347) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name"
+    t.string   "author"
+    t.string   "description"
+    t.integer  "rank"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,23 @@
 seed_movies = [
-  {name: "Rocky", director: "John G. Avildsen", description: "Creed beats Balboa", rank: 0},
-  {name: "Rocky II", director: "Sylvester Stallone", description: "Balboa beats Creed", rank: 0},
-  {name: "Rocky III", director: "Sylvester Stallone", description: "Balboa beats Lang", rank: 0},
-  {name: "Rocky IV", director: "Sylvester Stallone", description: "Balboa beats Drago", rank: 0},
-  {name: "Rocky V", director: "John G. Avildsen", description: "Balboa beats Gunn", rank: 0}
+  { name: "Rocky", director: "John G. Avildsen", description: "Creed beats Balboa", rank: 0 },
+  { name: "Rocky II", director: "Sylvester Stallone", description: "Balboa beats Creed", rank: 0 },
+  { name: "Rocky III", director: "Sylvester Stallone", description: "Balboa beats Lang", rank: 0 },
+  { name: "Rocky IV", director: "Sylvester Stallone", description: "Balboa beats Drago", rank: 0 },
+  { name: "Rocky V", director: "John G. Avildsen", description: "Balboa beats Gunn", rank: 0 }
 ]
 seed_movies.each do |seed|
   Movie.create(seed)
 end
 
-
+seed_books = [
+  { name: "Les Miserables", author: "Victor Hugo", description: "Desperation and redemption", rank: 0 },
+  { name: "Toilers of the Sea", author: "Victor Hugo", description: "Man vs. octopus", rank: 0 },
+  { name: "The Hunchback of Notre Dame", author: "Victor Hugo", description: "Quasimodo, the hopeless romantic", rank: 0 },
+  { name: "The Man Who Laughs", author: "Victor Hugo", description: "Gwynplaine, the hopeless romantic", rank: 0 }
+]
+seed_books.each do |seed|
+  Book.create(seed)
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
