d9ea6b0f0f229a8c7f170d282aa0b810571b29d5
Dreedle <davis.audrey.lynn@gmail.com>
2015-12-01 15:59:03 -0800

    Have rspec tests for getting each of the views for album

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe AlbumsController, type: :controller do
+  describe "GET 'index'" do
+    it "is successful" do
+      get :index
+      expect(response).to have_http_status 200
+    end
+  end
 
+  describe "GET 'new'" do
+    it "renders new view" do
+      get :new
+      expect(subject).to render_template :new
+    end
+  end
+
+  describe "GET 'edit'" do
+    let (:edit_album) do
+      Album.create(title:"a", artist: "b", description: "c", ranking: 0 )
+    end
+
+    it "renders edit view" do
+      get :edit, id: edit_album.id
+      expect(subject).to render_template :edit
+    end
+  end
+
+
+  describe "GET 'show'" do
+    let(:show_album) do
+      Album.create(title:"a", artist: "b", description: "c", ranking: 0 )
+    end
+
+    it "renders the show view" do
+      get :show, id: show_album.id
+      expect(subject).to render_template :show
+    end
+  end
+  #
+  # describe "POST 'create'" do
+  #   let(:good_params) do
+  #   {
+  #     post: {
+  #       body: "Something something something"
+  #     }
+  #   }
+  #   end
+  #
+  #   let (:bad_params) do
+  #     {
+  #       post: {}
+  #     }
+  #   end
+  #
+  #   it "redirects to index page" do
+  #     post :create, good_params
+  #
+  #     # Success case to index page
+  #     expect(subject).to redirect_to posts_path
+  #   end
+  #
+  #   it "renders new template on error" do
+  #     # Error case to show errors on form
+  #     post :create, bad_params
+  #     expect(subject).to render_template :new
+  #   end
+  # end
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe Album, type: :model do
       expect(Album.new(title: "t", ranking: 0)).to be_valid
     end
   end
+
+  # describe "top_ten" do
+  #   it "there are albums here" do
+  #     expect(Album.all.length).to_not eq(0)
+  #   end
+  #
+  #   it "returns just ten records" do
+  #     expect(Album.top_ten.length).to eq(10)
+  #   end
+  #
+  #   it "returns the highest ranked first" do
+  #     expect(Album.top_ten.first.ranking).to eq(Album.maximum(:ranking))
+  #   end
+  #
+  #   it "returns the lowest ranked last" do
+  #     expect(Album.top_ten.last.ranking).to eq(Album.minimum(:ranking))
+  #   end
+  # end
+
 end
