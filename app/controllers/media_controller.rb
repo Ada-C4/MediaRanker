e72b20bc79bc670dd5b802c6bf471fe0263b002a
Daphne <dg1181@nyu.edu>
2015-11-30 20:00:32 -0800

    media index up and running

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,2 +1,7 @@
 class MediaController < ApplicationController
+  def index
+    @movies = Movie.order(votes: :desc).first(10)
+    @books = Book.order(votes: :desc).first(10)
+    @albums = Album.order(votes: :desc).first(10)
+  end
 end
