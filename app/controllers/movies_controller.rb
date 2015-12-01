073c88e857cd99f96fa2d4f8222496b2ecba7731
Desiree <desireepoland@gmail.com>
2015-12-01 14:22:04 -0800

    Add create and update abilities to movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,23 +9,30 @@ class MoviesController < ApplicationController
  end
 
  def new
-
+   @movie = Movie.new
  end
 
  def edit
-
+   @movie = Movie.find(params[:id])
  end
 
  def create
-
+   @movie = Movie.new(movie_params)
+   if @movie.save
+     redirect_to movie_path(@movie.id)
+   else
+     render :new
+   end
  end
 
  def update
-
+   Movie.update(params[:id], movie_params)
+   redirect_to movie_path(params[:id])
  end
 
  def destroy
-
+  Movie.find(params[:id]).destroy
+  redirect_to movies_path
  end
 
  def upvote
