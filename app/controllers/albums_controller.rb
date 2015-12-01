b6982d4a112196b3ed67058b7f45a2870ec2454d
Rebecca Tolmach <rmtolmach@gmail.com>
2015-11-30 16:01:05 -0800

    page to make new album and show page to display it

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,24 @@
 class AlbumsController < ApplicationController
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.create(album_params)
+    redirect_to album_path(@album.id)
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  ###########PRIVATE############
+  private
+
+  def album_params
+    params.require(:album).permit(:name, :artist, :description)
+  end
+
+
 end
