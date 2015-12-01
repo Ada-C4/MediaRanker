e881c8b40826cd6a26ffa9529b2b3285821c2e17
Desiree <desireepoland@gmail.com>
2015-11-30 22:12:40 -0800

    Add empty methods to albums and movies controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,31 @@
 class AlbumsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
