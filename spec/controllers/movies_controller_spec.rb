fac8b33aea08c49accc658d6ddccd63bfb016a5c
Meighan Rasley <meighanr@gmail.com>
2015-12-01 15:08:58 -0800

    Add test for POST 'create'

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -9,4 +9,56 @@ RSpec.describe MoviesController, type: :controller do
       expect(response.status).to eq 200
     end
   end
+
+  describe "GET 'new'" do
+    it "renders a new view" do
+      get :new
+      expect(subject).to render_template :new
+    end
+  end
+
+  describe "GET 'show'" do
+    let(:movie) do
+      Movie.create(name: "Test", director: "Me", description: "boring film", rank: 7)
+    end
+
+    it "renders the show view" do
+      get :show, id: movie.id
+      expect(subject).to render_template :show
+    end
+  end
+
+  describe "POST 'create'" do
+    let(:movie) do
+      {
+        movie: {
+          name: "Test",
+          director: "Me",
+          description: "boring film",
+          rank: 7
+        }
+      }
+    end
+
+    let(:bad_movie) do
+      {
+        movie: {
+          name: "Test",
+          director: "Me",
+          description: nil,
+          rank: 7
+        }
+      }
+    end
+
+    it "redirects to movies index page" do
+      post :create, movie
+      expect(subject).to redirect_to movies_path
+    end
+
+    it "renders new template on error" do
+      post :create, bad_movie
+      expect(subject).to redirect_to movies_path
+    end
+  end
 end
