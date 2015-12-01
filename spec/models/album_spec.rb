649e27a95c6967d4eced75e3cdd9a50e9d2e87f7
Brittany <BrittanyKohler@gmail.com>
2015-12-01 11:41:08 -0800

    deleted unneccessary commented out stuff in forms

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-
+  describe ".validates" do
+    it "must have a name" do
+      expect(Album.new(name: "Brittany")).to be_valid
+    end
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-
+  describe ".validates" do
+    it "must have a name" do
+      expect(Movie.new(name: "Brittany")).to be_valid
+    end
+  end
 end
