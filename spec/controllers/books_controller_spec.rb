07305355f0dac24886e34fc6f91b666a423efcdc
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-01 13:43:11 -0800

    all book specs passing

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -3,14 +3,57 @@ require 'rails_helper'
 RSpec.describe BooksController, type: :controller do
 
 	describe BooksController do
+
 	  describe "GET 'index'" do
 
 	    it "is successful" do
 	      get :index
 	      expect(response.status).to eq 200
 	    end
-
 	  end
-	end
 
+	  describe "GET 'show'" do
+	  	let(:book) do
+	  		Book.create(name: 'a book')
+	  	end
+	  	it "renders the show view" do
+	  		get :show, id: book.id
+	  		expect(response.status).to eq 200
+	  	end
+	  end
+
+    describe "POST 'create'" do
+    	let(:params) do
+    		{
+    			books: 
+    			{
+    				name: "this is a name"
+    			}
+    		}
+    	end
+
+    	let(:bad_params) do
+    		{
+    			books:
+    			{
+    				description: 'this'
+    			}
+    		}
+    	end
+
+    	# works properly
+    	it "redirects to index page" do
+    		post :create, params
+    		expect(subject).to redirect_to book_path(1)
+    	end
+
+    	# error creation
+    	it "renders new template" do
+    		post :create, bad_params
+    		expect(subject).to render_template("new")
+    	end
+    end
+
+
+	end
 end
