ae2418951bed685de2dd7dd418122aabb9589d4e
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-01 11:14:36 -0800

    added specs for model validations

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  describe ".validates" do
+    it "must have a name" do
+      expect(Book.new(name: nil)).to be_invalid
+    end
+  end
+
+end
