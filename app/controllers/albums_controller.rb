0a5ee902a4115c6f66f7e0709f983f18a397981d
Amy Hunter <amyrhunter@gmail.com>
2015-12-01 15:44:16 -0800

    Correct column name to artist, create and make methods in albums controller

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,52 @@
+class AlbumsController < ApplicationController
+  before_action :get_album, only: [:edit, :update, :destroy]
+  def get_album
+    @album = Album.find(params[:id])
+  end
+
+  def index
+    @albums = Album.all
+  end
+
+  def new
+    @url = "create"
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.create(album_params[:album])
+    redirect_to "/albums"
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  def edit
+    @legend = "Edit Album"
+  end
+
+  def update
+    Album.update(params[:id], album_params[:album])
+    redirect_to "/albums/#{@album.id}"
+  end
+
+  def destroy
+    Album.destroy(params[:id])
+    redirect_to albums_path
+  end
+
+  def upvote
+    @album = Album.find(params[:id])
+    @album.rank += 1
+    @album.save
+    render "show"
+  end
+
+private
+
+  def album_params
+    params.permit(album: [:name, :artist, :description, :rank])
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201220347) do
+ActiveRecord::Schema.define(version: 20151201230438) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "name"
+    t.string   "artist"
+    t.string   "description"
+    t.integer  "rank"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
