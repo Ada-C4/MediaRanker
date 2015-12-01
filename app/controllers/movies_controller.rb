b88e7bdbdeba361d85679f11aea4870431dadd6b
Meighan Rasley <meighanr@gmail.com>
2015-11-30 16:20:22 -0800

    Add data to movie index view and update new view form_for with director field

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,14 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    @movie = Movie.create(movie_params[:movie])
+    redirect_to "/movies"
+  end
+
+  private
+
+  def movie_params
+    params.permit(movie:[:name, :director, :description, :rank])
+  end
 end
