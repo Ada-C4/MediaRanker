babed9150540e94b215c044489a53e84862903d7
Tammy <misstamar@gmail.com>
2015-12-01 08:04:24 -0800

    completed 3 models most view and controllers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,36 @@
 class MoviesController < ApplicationController
 
   def get_movie
-    @movie = Movie.find(params[:movie_id])
+    @movie = Movie.find(params[:id])
   end
 
   def index
+    @movies = Movie.all
   end
 
   def new
+    @movie = Movie.new
   end
 
   def show
     get_movie
   end
 
+  def edit
+    get_movie
+  end
+
   def update
+    if params[:class] == "upvote"
+      get_movie
+      rank = @movie[:rank] + 1
+      @movie.update(:rank => rank)
+      render "show"
+    else
+      get_movie
+      @movie.update(movie_params[:movie])
+      redirect_to movie_path
+    end
   end
 
   def create
@@ -26,6 +42,6 @@ class MoviesController < ApplicationController
   private
 
   def movie_params
-    params.permit(:movie[:name, :director, :description])
+    params.permit(movie: [:name, :director, :description, :rank])
   end
 end
