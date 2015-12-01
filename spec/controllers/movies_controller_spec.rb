96c2505e2a61cc4995e48d8e117090eccb687428
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-01 13:59:54 -0800

    added specs for movie index and show methods

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
+  describe "GET 'index'" do
+    it "renders the index view" do
+      get :index
+      expect(response.status).to eq 200
+    end
+  end
 
+  describe "GET 'show'" do
+    let(:movie) do
+      Movie.create(name: "Test")
+    end
+
+    it "renders the show view" do
+      get :show, id: movie.id
+      expect(response.status).to eq 200
+    end
+  end
 end
