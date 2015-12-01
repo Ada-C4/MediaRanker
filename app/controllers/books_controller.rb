48a886580b36dabeeec3e5923e812f247db9bb15
Daphne <dg1181@nyu.edu>
2015-11-30 18:41:40 -0800

    Books has full functionality now

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,70 @@
 class BooksController < ApplicationController
+  def index
+    @items = Book.order(votes: :desc)
+    @type = "Book"
+
+    render "shared/index"
+  end
+
+  def show
+    @item = Book.find(params[:id])
+    @type = "Books"
+    @special = "Written"
+
+
+    render "shared/show"
+  end
+
+  def upvote
+    Book.find(params[:id]).increment!(:votes)
+
+    redirect_to :back
+  end
+
+  def new
+    @item = Book.new
+    @special = "Author"
+
+    render "shared/new"
+  end
+
+  def create
+    @item = Book.new(book_params)
+
+    if @item.save
+      redirect_to books_path
+    else
+      render "shared/new"
+    end
+  end
+
+  def edit
+    @item = Book.find(params[:id])
+    @special = "Author"
+
+    render "shared/new"
+  end
+
+  def update
+    @item = Book.find(params[:id])
+    @item.attributes = book_params
+
+    if @item.save
+      redirect_to books_path
+    else
+      render "shared/new"
+    end
+  end
+
+  def destroy
+    Book.destroy(Book.find(params[:id]))
+
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :creator, :description)
+  end
 end
