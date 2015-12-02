909782e89fb8390c90b855d71a5ddcc33ba7c8a1
Jenna Nichols <jennaplusplus@gmail.com>
2015-12-01 16:35:42 -0800

    added top books and albums to front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,11 @@ class WelcomeController < ApplicationController
   def index
     movies = Movie.all
     @movies = movies.order(votes: :desc).first(10)
-  
+
+    books = Book.all
+    @books = books.order(votes: :desc).first(10)
+    
+    albums = Album.all
+    @albums = albums.order(votes: :desc).first(10)
   end
 end
