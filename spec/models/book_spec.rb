d7312b20520fa8e3c1b023877a76c730bdec1b92
Meighan Rasley <meighanr@gmail.com>
2015-12-02 15:18:29 -0800

    Add name validates test to book_spec.rb

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  describe ".validates" do
+    it "must have a name" do
+      expect(Book.new(name: nil)).to_not be_valid
+    end
 end
