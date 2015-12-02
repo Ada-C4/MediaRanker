ebb0e44515685c668420208b2f25fca45b5a64f6
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-02 15:33:56 -0800

    bootstrapping done, sites look similar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def all_media
   	# x is the number of items that will show in the 'top' display
-  	x = 2
+  	x = 10
   	@top_movies = Movie.order(votes: :desc).limit(x)
   	@top_books = Book.order(votes: :desc).limit(x)
   	@top_albums = Album.order(votes: :desc).limit(x)
