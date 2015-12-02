17c5241ac3d0d39b8ee3f2583e0bed6de0caf35c
Meighan Rasley <meighanr@gmail.com>
2015-12-02 14:56:15 -0800

    Create book model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,3 @@
 class Movie < ActiveRecord::Base
   validates :name, presence: true
-  validates :director, presence: true
-  validates :description, presence: true
-  validates :rank, presence: true, numericality: true
 end
