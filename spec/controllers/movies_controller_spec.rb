05ecd42b19f10a1ab890ff0fb179901a8ccfdfaf
emgord <emgord@gmail.com>
2015-12-01 16:36:01 -0800

    adding specs for movie controller

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -14,4 +14,86 @@ RSpec.describe MoviesController, type: :controller do
         expect(subject).to render_template :new
     end
   end
+
+  describe "POST 'create'" do
+    let(:good_params) do
+      {
+        movie:{
+          name: "Hello", director: "You", description: "A Good Movie", rank: 0
+        }
+      }
+    end
+    let(:bad_params) do
+      {
+      movie:{
+       director: "You", description: "A Good Movie", rank: 0
+      }
+    }
+    end
+    it "redirects to show page" do
+      post :create, good_params
+      expect(subject).to redirect_to movie_path(assigns(:movie))
+    end
+    it "renders new template on error" do
+      post :create, bad_params
+      expect(subject).to render_template :new
+    end
+  end
+
+  describe "GET 'show'" do
+    test_movie = Movie.create( name: "Hello", director: "You", description: "A Good Movie", rank: 0 )
+
+    it "renders the show view" do
+      get :show, id: test_movie.id
+      expect(subject).to render_template :show
+    end
+  end
+
+  describe "GET 'edit'" do
+    let(:test_movie){
+      Movie.create( name: "Hello", director: "You", description: "A Good Movie", rank: 0 )
+    }
+    it "renders edit view" do
+      get :edit, id: test_movie.id
+      expect(subject).to render_template :edit
+  end
+end
+
+describe "PATCH 'update'" do
+  let(:good_params) do
+    {
+      id: 1,
+      movie:{
+        name: "Hello", director: "You", description: "A Good Movie", rank: 0
+      }
+    }
+  end
+
+  let(:bad_params) do
+    {
+      id: 1,
+      movie: { name: nil, director: "You", description: "A Good Movie", rank: 0 }
+  }
+  end
+  it "redirects to show page" do
+    patch :update, good_params
+    expect(subject).to redirect_to movie_path(assigns(:movie))
+  end
+  it "renders edit template on error" do
+    patch :update, bad_params
+    expect(subject).to render_template :edit
+  end
+end
+
+describe "DELETE 'destroy'" do
+  let(:test_movie){
+    Movie.create( name: "Hello", director: "You", description: "A Good Movie", rank: 0 )
+  }
+
+  it "redirects to the index view" do
+    delete :destroy, id: test_movie.id
+    expect(subject).to redirect_to movies_path
+  end
+end
+
 end
