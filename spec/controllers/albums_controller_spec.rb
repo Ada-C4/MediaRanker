99f77d2ff17a3dbf9ea5e60a21d1691fb7a63662
Desiree <desireepoland@gmail.com>
2015-12-01 16:03:27 -0800

    Add spec for POST 'create' when good params

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -15,7 +15,36 @@ RSpec.describe AlbumsController, type: :controller do
 
     it "renders show view" do
       get :show, id: album.id
+      expect(subject).to render_template :show
+    end
+
+    it "is successful" do
+      get :show, id: album.id
       expect(response.status).to eq 200
+    end
+  end
+
+  describe "POST 'create'" do
+    let(:good_params) do
+      {
+        album: {
+          name: "Some Album Name"
+        }
+      }
+    end
+
+    let(:bad_params) do
+      {
+        album: {
+          description: "nameless album"
+        }
+      }
+    end
+
+    it "redirects to show page" do
+      post :create, good_params
+      #Success case to show page
+      expect(subject).to redirect_to album_path(1)
     end
   end
 end
