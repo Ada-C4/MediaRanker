a87ff828d7c05cbb31d993fc2da33bf5528e4424
Desiree <desireepoland@gmail.com>
2015-12-01 16:52:44 -0800

    PATCH update spec for albums

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -49,7 +49,32 @@ RSpec.describe AlbumsController, type: :controller do
     end
 
     it "renders new template on error" do
-      
+      # Error case to show errors on form
+      post :create, bad_params
+      expect(subject).to render_template :new
     end
   end
+
+  describe "PATCH 'update'" do
+
+    it "redirects to show page" do
+      album = Album.create(name: "Album", artist:"Artist", description:"Description")
+      params = {
+        album:{
+          name: "something else",
+          artist: "someone",
+          description:"blablabla"
+        },
+        id: album.id
+      }
+      patch :update, params
+      expect(subject).to redirect_to album_path(album.id)
+    end
+
+    # it "renders edit template on error" do
+    #   patch :update, bad_params
+    #   expect(subject).to render_template :edit
+    # end
+  end
+
 end
