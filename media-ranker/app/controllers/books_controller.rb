9b4c3d0be14329a84a184e65d637f299524d1fcf
Lauren Granger <lr.granger21@gmail.com>
2015-12-03 11:31:48 -0800

    for books controller: routes, methods, and specs for RESTful routes + edit, index, new, and show views

--- media-ranker/app/controllers/books_controller.rb
+++ media-ranker/app/controllers/books_controller.rb
@@ -1,7 +1,27 @@
 class BooksController < ApplicationController
 
   def index
+  end
 
+  def create
+    redirect_to books_path
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    redirect_to books_path
+  end
+
+  def destroy
+    redirect_to books_path
   end
 
 end
