261b85b3d24a72a5a4095c7ed6966c78c239e664
Jenna Nichols <jennaplusplus@gmail.com>
2015-12-03 10:24:48 -0800

    shared examples for Book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,29 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  it_behaves_like "a medium"
+
   describe ".validates" do
-    it "must have a title" do
-      expect(Book.new(title: nil)).to be_invalid
-    end
-
-    it "must have a unique title" do
-      Book.create(title: "a")
-      expect(Book.new(title: "a")).to be_invalid
-    end
-
-    it "cannot have a title with more than 100 characters" do
-      expect(Book.new(title: "a" * 100)).to be_valid
-      expect(Book.new(title: "a" * 101)).to be_invalid
-    end
-
     it "cannot have an author with more than 100 characters" do
       expect(Book.new(title: "a", author: "a" * 100)).to be_valid
       expect(Book.new(title: "a", author: "a" * 101)).to be_invalid
-    end
-
-    it "cannot have a description with more than 1000 characters" do
-      expect(Book.new(title: "a", description: "a" * 1000)).to be_valid
-      expect(Book.new(title: "a", description: "a" * 1001)).to be_invalid
     end
   end
 end
