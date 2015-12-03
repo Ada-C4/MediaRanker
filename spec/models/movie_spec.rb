48a93062c5ab4ea677a59210e43093a9f0f7ca3b
emgord <emgord@gmail.com>
2015-12-03 10:56:56 -0800

    successfully created shared spec for movie

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  describe ".validates" do
-    it "must have a name" do
-      expect(Movie.new(name: nil)).to_not be_valid
-    end
-  end
-
-
-
+  it_behaves_like "a medium"
 
 end
---
+++ spec/support/medium_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.shared_examples "a medium" do
+  describe ".validates" do
+    it "must have a name" do
+      expect(described_class.new(name: nil)).to_not be_valid
+    end
+  end
+end
