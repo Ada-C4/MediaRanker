0dcbbe2b401ce1b9e6e4d68b6658bc56e12248c6
Brittany <BrittanyKohler@gmail.com>
2015-12-03 12:13:59 -0800

    Fixed controller specs for album and movies to use shared spec

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -1,110 +1,139 @@
 require 'rails_helper'
 
 RSpec.describe AlbumsController, type: :controller do
-  describe "GET 'index'" do
-      it "is successful" do
-        get :index
-        expect(response.status).to eq 200
-      end
+  it_behaves_like "a medium controller" do
+    let(:medium) do
+      Album.create(name: "Something", artist: "Someone", description: "Something something something")
     end
-
-    describe "GET 'new'" do
-      it "renders new view" do
-        get :new
-        expect(subject).to render_template :new
-      end
+    let(:medium2) do
+      Album.create(name: "Something", artist: "Someone", description: "Something something something", ranking: 3)
     end
-
-    describe "GET 'show'" do
-      let(:album) do
-        Album.create(name: "Test")
-      end
-
-      it "renders the show view" do
-        get :show, id: album.id
-        expect(subject).to render_template :show
-      end
+    let(:model) do
+      "Album".constantize
     end
-
-    describe "POST 'create'" do
-      let(:good_params) do
-      {
-        album: {
-          name: "Something something something"
-        }
+    let(:good_params) do
+    {
+      album: {
+        name: "Something something something"
       }
-      end
-
-      let (:bad_params) do
-        {
-          album: {}
-        }
-      end
-
-      it "redirects to index page" do
-
-        post :create, good_params
-        new_album = Album.last
-
-        # Success case to index page
-        expect(subject).to redirect_to album_path(new_album.id)
-      end
-
-      it "renders new template on error" do
-        # Error case to show errors on form
-        post :create, bad_params
-        expect(subject).to render_template :new
-      end
+    }
     end
-
-    describe "GET 'edit'" do
-      let(:album) do
-        Album.create(name: "Test")
-      end
-
-      it "renders the edit view" do
-        get :edit, id: album.id
-        expect(subject).to render_template :edit
-      end
+    let(:update_params) do
+    {
+      album: {
+        name: "Something",
+        author: "Someone",
+        description: "Something something something"
+      },
+      id: medium.id
+    }
     end
+  end
 
-    describe "PATCH 'update'" do
-      it "redirects to show page" do
-        album = Album.create(name: "Something", artist: "Someone", description: "Something something something")
-        params =   {
-          album: {
-            name: "Something",
-            artist: "Someone",
-            description: "Something something something"
-          },
-          id: album.id
-        }
-        patch :update, params
-        expect(subject).to redirect_to album_path(album.id)
-      end
-    end
-
-    describe "DELETE 'destroy'" do
-      let(:album) do
-        Album.create(name: "Test")
-      end
-
-      it "redirects to index page" do
-        delete :destroy, id: album.id
-        expect(subject).to redirect_to albums_path
-      end
-    end
-
-    describe "POST 'upvote'" do
-      it "redirects to show page" do
-        album = Album.create(name: "Something", artist: "Someone", description: "Something something something")
-        post :upvote, id: album.id
-        expect(subject).to redirect_to album_path(album.id)
-      end
-      it "redirects to show page" do
-        album = Album.create(name: "Something", artist: "Someone", description: "Something something something", ranking: 3)
-        post :upvote, id: album.id
-        expect(subject).to redirect_to album_path(album.id)
-      end
-    end
+  # describe "GET 'index'" do
+  #     it "is successful" do
+  #       get :index
+  #       expect(response.status).to eq 200
+  #     end
+  #   end
+  #
+  #   describe "GET 'new'" do
+  #     it "renders new view" do
+  #       get :new
+  #       expect(subject).to render_template :new
+  #     end
+  #   end
+  #
+  #   describe "GET 'show'" do
+  #     let(:album) do
+  #       Album.create(name: "Test")
+  #     end
+  #
+  #     it "renders the show view" do
+  #       get :show, id: album.id
+  #       expect(subject).to render_template :show
+  #     end
+  #   end
+  #
+  #   describe "POST 'create'" do
+  #     let(:good_params) do
+  #     {
+  #       album: {
+  #         name: "Something something something"
+  #       }
+  #     }
+  #     end
+  #
+  #     let (:bad_params) do
+  #       {
+  #         album: {}
+  #       }
+  #     end
+  #
+  #     it "redirects to index page" do
+  #
+  #       post :create, good_params
+  #       new_album = Album.last
+  #
+  #       # Success case to index page
+  #       expect(subject).to redirect_to album_path(new_album.id)
+  #     end
+  #
+  #     it "renders new template on error" do
+  #       # Error case to show errors on form
+  #       post :create, bad_params
+  #       expect(subject).to render_template :new
+  #     end
+  #   end
+  #
+  #   describe "GET 'edit'" do
+  #     let(:album) do
+  #       Album.create(name: "Test")
+  #     end
+  #
+  #     it "renders the edit view" do
+  #       get :edit, id: album.id
+  #       expect(subject).to render_template :edit
+  #     end
+  #   end
+  #
+  #   describe "PATCH 'update'" do
+  #     it "redirects to show page" do
+  #       album = Album.create(name: "Something", artist: "Someone", description: "Something something something")
+  #       params =   {
+  #         album: {
+  #           name: "Something",
+  #           artist: "Someone",
+  #           description: "Something something something"
+  #         },
+  #         id: album.id
+  #       }
+  #       patch :update, params
+  #       expect(subject).to redirect_to album_path(album.id)
+  #     end
+  #   end
+  #
+  #   describe "DELETE 'destroy'" do
+  #     let(:album) do
+  #       Album.create(name: "Test")
+  #     end
+  #
+  #     it "redirects to index page" do
+  #       delete :destroy, id: album.id
+  #       expect(subject).to redirect_to albums_path
+  #     end
+  #   end
+  #
+  #   describe "POST 'upvote'" do
+  #     it "redirects to show page" do
+  #       album = Album.create(name: "Something", artist: "Someone", description: "Something something something")
+  #       post :upvote, id: album.id
+  #       expect(subject).to redirect_to album_path(album.id)
+  #     end
+  #     it "redirects to show page" do
+  #       album = Album.create(name: "Something", artist: "Someone", description: "Something something something", ranking: 3)
+  #       post :upvote, id: album.id
+  #       expect(subject).to redirect_to album_path(album.id)
+  #     end
+  #   end
 end
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,32 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
-  describe "GET 'index'" do
-    it "is successful" do
-      get :index
-      expect(response.status).to eq 200
+  it_behaves_like "a medium controller" do
+    let(:medium) do
+      Movie.create(name: "Something", director: "Someone", description: "Something something something")
     end
-  end
-
-  describe "GET 'new'" do
-    it "renders new view" do
-      get :new
-      expect(subject).to render_template :new
+    let(:medium2) do
+      Movie.create(name: "Something", director: "Someone", description: "Something something something", ranking: 3)
     end
-  end
-
-  describe "GET 'show'" do
-    let(:movie) do
-      Movie.create(name: "Test")
+    let(:model) do
+      "Movie".constantize
     end
-
-    it "renders the show view" do
-      get :show, id: movie.id
-      expect(subject).to render_template :show
-    end
-  end
-
-  describe "POST 'create'" do
     let(:good_params) do
     {
       movie: {
@@ -34,77 +18,122 @@ RSpec.describe MoviesController, type: :controller do
       }
     }
     end
-
-    let (:bad_params) do
-      {
-        movie: {}
-      }
-    end
-
-    it "redirects to index page" do
-
-      post :create, good_params
-      new_movie = Movie.last
-
-      # Success case to index page
-      expect(subject).to redirect_to movie_path(new_movie.id)
-    end
-
-    it "renders new template on error" do
-      # Error case to show errors on form
-      post :create, bad_params
-      expect(subject).to render_template :new
+    let(:update_params) do
+    {
+      movie: {
+        name: "Something",
+        director: "Someone",
+        description: "Something something something"
+      },
+      id: medium.id
+    }
     end
   end
-
-  describe "GET 'edit'" do
-    let(:movie) do
-      Movie.create(name: "Test")
-    end
-
-    it "renders the edit view" do
-      get :edit, id: movie.id
-      expect(subject).to render_template :edit
-    end
-  end
-
-  describe "PATCH 'update'" do
-    it "redirects to show page" do
-      movie = Movie.create(name: "Something", director: "Someone", description: "Something something something")
-      params =   {
-        movie: {
-          name: "Something",
-          author: "Someone",
-          description: "Something something something"
-        },
-        id: movie.id
-      }
-      patch :update, params
-      expect(subject).to redirect_to movie_path(movie.id)
-    end
-  end
-
-  describe "DELETE 'destroy'" do
-    let(:movie) do
-      Movie.create(name: "Test")
-    end
-
-    it "redirects to index page" do
-      delete :destroy, id: movie.id
-      expect(subject).to redirect_to movies_path
-    end
-  end
-
-  describe "POST 'upvote'" do
-    it "redirects to show page when ranking is nil" do
-      movie = Movie.create(name: "Something", director: "Someone", description: "Something something something")
-      post :upvote, id: movie.id
-      expect(subject).to redirect_to movie_path(movie.id)
-    end
-    it "redirects to show page when ranking is not nil" do
-      movie = Movie.create(name: "Something", director: "Someone", description: "Something something something", ranking: 3)
-      post :upvote, id: movie.id
-      expect(subject).to redirect_to movie_path(movie.id)
-    end
-  end
+  #
+  # describe "GET 'index'" do
+  #   it "is successful" do
+  #     get :index
+  #     expect(response.status).to eq 200
+  #   end
+  # end
+  #
+  # describe "GET 'new'" do
+  #   it "renders new view" do
+  #     get :new
+  #     expect(subject).to render_template :new
+  #   end
+  # end
+  #
+  # describe "GET 'show'" do
+  #   let(:movie) do
+  #     Movie.create(name: "Test")
+  #   end
+  #
+  #   it "renders the show view" do
+  #     get :show, id: movie.id
+  #     expect(subject).to render_template :show
+  #   end
+  # end
+  #
+  # describe "POST 'create'" do
+  #   let(:good_params) do
+  #   {
+  #     movie: {
+  #       name: "Something something something"
+  #     }
+  #   }
+  #   end
+  #
+  #   let (:bad_params) do
+  #     {
+  #       movie: {}
+  #     }
+  #   end
+  #
+  #   it "redirects to index page" do
+  #
+  #     post :create, good_params
+  #     new_movie = Movie.last
+  #
+  #     # Success case to index page
+  #     expect(subject).to redirect_to movie_path(new_movie.id)
+  #   end
+  #
+  #   it "renders new template on error" do
+  #     # Error case to show errors on form
+  #     post :create, bad_params
+  #     expect(subject).to render_template :new
+  #   end
+  # end
+  #
+  # describe "GET 'edit'" do
+  #   let(:movie) do
+  #     Movie.create(name: "Test")
+  #   end
+  #
+  #   it "renders the edit view" do
+  #     get :edit, id: movie.id
+  #     expect(subject).to render_template :edit
+  #   end
+  # end
+  #
+  # describe "PATCH 'update'" do
+  #   it "redirects to show page" do
+  #     movie = Movie.create(name: "Something", director: "Someone", description: "Something something something")
+  #     params =   {
+  #       movie: {
+  #         name: "Something",
+  #         author: "Someone",
+  #         description: "Something something something"
+  #       },
+  #       id: movie.id
+  #     }
+  #     patch :update, params
+  #     expect(subject).to redirect_to movie_path(movie.id)
+  #   end
+  # end
+  #
+  # describe "DELETE 'destroy'" do
+  #   let(:movie) do
+  #     Movie.create(name: "Test")
+  #   end
+  #
+  #   it "redirects to index page" do
+  #     delete :destroy, id: movie.id
+  #     expect(subject).to redirect_to movies_path
+  #   end
+  # end
+  #
+  # describe "POST 'upvote'" do
+  #   it "redirects to show page when ranking is nil" do
+  #     movie = Movie.create(name: "Something", director: "Someone", description: "Something something something")
+  #     post :upvote, id: movie.id
+  #     expect(subject).to redirect_to movie_path(movie.id)
+  #   end
+  #   it "redirects to show page when ranking is not nil" do
+  #     movie = Movie.create(name: "Something", director: "Someone", description: "Something something something", ranking: 3)
+  #     post :upvote, id: movie.id
+  #     expect(subject).to redirect_to movie_path(movie.id)
+  #   end
+  # end
 end
