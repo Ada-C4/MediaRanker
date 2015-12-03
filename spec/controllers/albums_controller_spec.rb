8b02dd03ec9f122e84452f0c8fb2016ee383ae79
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-03 11:29:05 -0800

    fixed update validation test

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -1,100 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe AlbumsController, type: :controller do
-
 	let(:create_params) do {
-		album: {
-			name: "Some Album",
-			description: "It's got music!",
-			votes: 1
-		}
-	}
-end
-
-	let(:album) { Album.create(create_params[:album]) }
-
-	describe AlbumsController do
-	  describe "GET 'index'" do
-
-	    it "is successful" do
-	      get :index
-	      expect(response.status).to eq 200
-	    end
-	  end
-
-	  describe "GET 'new'" do
-	  	it "renders the 'new' view" do
-	  		get :new
-	  		expect(subject).to render_template("new")
-	  	end
-	  end
-
-	  describe "GET 'show'" do
-	  	it "renders the show view" do
-	  		get :show, id: album.id
-	  		expect(response.status).to eq 200
-	  	end
-	  end
-
-	  describe "POST 'create'" do
-
-	  	let(:bad_params) do
-	  		{ 
-	  			album:
-	  			{
-	  				description: 'this'
-	  			}
-	  		}
-	  	end
-
-	  	# item created properly
-	  	it "redirects to show page" do
-	  		post :create, create_params
-	  		expect(subject).to redirect_to album_path(1)
-	  	end
-
-	  	# error in creation
-	  	it "renders new template" do
-	  		post :create, bad_params
-	  		expect(subject).to render_template("new")
-	  	end
-	  end
-
-	 	describe "PATCH 'update'" do
-			let(:album_id) { album.id }
-			let(:update_params) do 
-				{
-						name: "Some Album",
-						description: "It's got music that I don't like :("
+				album: {
+					name: "Some Album",
+					description: "It's got music!",
+					votes: 1
 				}
-			end
-			it "should should be successful" do
-				patch :update, { id: album_id, album: update_params }
-				expect(subject).to redirect_to album_path(album_id)
-			end
-			it "should reload the page if not validated" do
-				patch :update, { id: album_id, album: {name: nil}}
-				expect(subject).to redirect_to edit_album_path(album_id)
-			end
+			}
 		end
 
-		describe "PATCH 'upvote'" do
-			before(:each) do
-			  request.env["HTTP_REFERER"] = 'back'
-			end
+	let(:media) { Album.create(create_params[:album]) }
 
-			it "should redirect to same page" do
-				patch :upvote, id: album.id
-				expect(subject).to redirect_to 'back'
-			end
+	let(:bad_params) do
+		{ 
+			id: media.id,
+			album: {
+				name: nil,
+				description: 'this'
+			}
+		}
 		end
 
-  	describe "DELETE 'destroy'" do
-  		it "should redirect to albums page" do
-			  delete :destroy, id: album.id
-	  		expect(subject).to redirect_to albums_path
-  		end
-  	end
-
+	let(:update_params) do 
+		{
+			id: media.id,
+			album: {
+				name: "Some Thing",
+				description: "here's a new thing"
+			}
+		}
 	end
+
+	it_behaves_like "a media controller"
+
 end
--- spec/support/media_controller_spec.rb
+++ spec/support/media_controller_spec.rb
@@ -1,1 +1,70 @@
 require 'rails_helper'
+
+RSpec.shared_examples "a media controller" do
+	include Rails.application.routes.url_helpers
+	
+  describe "GET 'index'" do
+
+    it "is successful" do
+      get :index
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe "GET 'new'" do
+  	it "renders the 'new' view" do
+  		get :new
+  		expect(subject).to render_template("new")
+  	end
+  end
+
+  describe "GET 'show'" do
+  	it "renders the show view" do
+  		get :show, id: media.id
+  		expect(response.status).to eq 200
+  	end
+  end
+
+  describe "POST 'create'" do
+  	# item created properly
+  	it "redirects to show page" do
+			post :create, create_params
+  		expect(subject).to redirect_to polymorphic_path(media)
+  	end
+
+  	# error in creation
+  	it "renders new template" do
+  		post :create, bad_params
+  		expect(subject).to render_template("new")
+  	end
+  end
+
+ 	describe "PATCH 'update'" do
+		it "should should be successful" do
+			patch :update, update_params
+			expect(subject).to redirect_to polymorphic_path(media)
+		end
+		it "should reload the page if not validated" do
+			patch :update, bad_params
+			expect(subject).to redirect_to edit_polymorphic_path(media)
+		end
+	end
+
+	describe "PATCH 'upvote'" do
+		before(:each) do
+		  request.env["HTTP_REFERER"] = 'back'
+		end
+
+		it "should redirect to same page" do
+			patch :upvote, id: media.id
+			expect(subject).to redirect_to 'back'
+		end
+	end
+
+	describe "DELETE 'destroy'" do
+		it "should redirect to media show page" do
+		  delete :destroy, id: media.id
+  		expect(subject).to redirect_to albums_path
+		end
+	end
+end
