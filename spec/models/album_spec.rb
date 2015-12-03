91d484da67e45c8f34a0eb09221eb54c871efc00
Brittany <BrittanyKohler@gmail.com>
2015-12-03 10:22:41 -0800

    Adding shared test for model

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe Album, type: :model do
   it_behaves_like "a medium" do
 
-  
-
     describe ".validates" do
       it "must have a name" do
         expect(Album.new(name: "Brittany")).to be_valid
--- spec/support/medium_spec.rb
+++ spec/support/medium_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.shared_examples "a medium" do
+  describe ".validates" do
+    it "requires a name" do
+      medium = described_class.new
 
+      expect(medium).to be_invalid
+      expect(medium.errors.keys).to include :name
+    end
+
+    # it "must have a name" do
+    #   expect(Album.new(name: "Brittany")).to be_valid
+    # end
+  end
 end
