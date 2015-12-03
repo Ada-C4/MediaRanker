c4a66987820a5fc3505cab617c0c858af2447e7f
Meighan Rasley <meighanr@gmail.com>
2015-12-03 14:26:15 -0800

    Code in home controller index to show the top ten movies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   def index
     @movies = Movie.all
     @movies_sort = @movies.sort_by{ |movie| movie[:rank] }.reverse
+    @movies_ten = @movies.first(10)
   end
 end
