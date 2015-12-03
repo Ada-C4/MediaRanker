6641959b2c6c6a019bac68fc6d2cc640a03fc3da
Desiree <desireepoland@gmail.com>
2015-12-03 14:48:46 -0800

    Add specs to movies controller

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -18,4 +18,116 @@ RSpec.describe MoviesController, type: :controller do
       expect(response.status).to eq 200
     end
   end
+
+  describe "GET 'new'" do
+    it "is successful" do
+      get :new
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET 'edit'" do
+    let(:movie) do
+      Movie.create(name: "name", director:"someone", description:"desc")
+    end
+    it "is successful" do
+      get :edit, id: movie.id
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe "POST 'create'" do
+    let(:good_params) do
+      {
+        movie: {
+          name: "name",
+          description: "description"
+        }
+      }
+    end
+
+    let(:bad_params) do
+      {
+        movie: {
+          name: nil,
+          description: "something"
+        }
+      }
+    end
+
+    it "redirects to show page" do
+      post :create, good_params
+      #Success case to show page
+      new_movie = Movie.last
+      expect(subject).to redirect_to movie_path(new_movie.id)
+    end
+
+    it "renders new template on error" do
+      # Error case to show errors on form
+      post :create, bad_params
+      expect(subject).to render_template :new
+    end
+  end
+
+  describe "PATCH 'update'" do
+    let(:movie) do
+      Movie.create(name: "movie", director:"name", description:"Description")
+    end
+
+    let(:good_params) do
+      {
+          movie:{
+            name: "movie1",
+            director: "name1",
+            description:"desc1"
+          },
+          id: movie.id
+      }
+    end
+
+    let(:bad_params) do
+      {
+          movie:{
+            name: nil,
+            director: "name2",
+            description:"desc2"
+          },
+          id: movie.id
+      }
+    end
+
+    it "redirects to show page" do
+      patch :update, good_params
+      expect(subject).to redirect_to movie_path(movie.id)
+    end
+
+    it "renders edit template on error" do
+      patch :update, bad_params
+      expect(subject).to render_template :edit
+    end
+  end
+
+  describe "DELETE 'destroy'" do
+    let(:movie) do
+      Movie.create(name: "name")
+    end
+
+    it "redirects to index page" do
+      delete :destroy, id: movie.id
+      expect(subject).to redirect_to(movies_path)
+    end
+  end
+
+  describe "POST 'upvote'" do
+    let(:movie) do
+      Movie.create(name: "name", upvotes: 0)
+    end
+
+    it "incremements movie upvotes by 1" do
+      post :upvote, id: movie.id
+      movie.reload
+      expect(movie.upvotes).to eq 1
+    end
+  end
+
 end
