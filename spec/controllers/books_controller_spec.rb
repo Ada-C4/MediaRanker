8fdcaa0515b5350e48d5f27dbea6a854227b5245
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-03 11:47:26 -0800

    shared examples working for all

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -3,111 +3,40 @@ require 'rails_helper'
 RSpec.describe BooksController, type: :controller do
 
 	let(:create_params) do {
+				book: {
+					name: "Some book",
+					description: "It's got music!",
+					votes: 1
+				}
+			}
+		end
+
+	let(:media) { Book.create(create_params[:book]) }
+
+	let(:medias) {Book.all}
+
+	let(:bad_params) do
+		{ 
+			id: media.id,
 			book: {
-				name: "Some book",
-				description: "It's got music!",
-				votes: 1,
+				name: nil,
+				description: 'this'
+			}
+		}
+		end
+
+	let(:update_params) do 
+		{
+			id: media.id,
+			book: {
+				name: "Some Thing",
+				description: "here's a new thing"
 			}
 		}
 	end
 
-	let(:book) { Book.create(create_params[:book]) }
+	let(:model) {Book}
+	let(:medias) {"books"}
 
-	describe BooksController do
-
-	  describe "GET 'index'" do
-
-	    it "is successful" do
-	      get :index
-	      expect(response.status).to eq 200
-	    end
-	  end
-
-	  describe "GET 'new'" do
-	  	it "renders the 'new' view" do
-	  		get :new
-	  		expect(subject).to render_template("new")
-	  	end
-	  end
-
-	  describe "GET 'show'" do
-	  	let(:book) do
-	  		Book.create(name: 'a book')
-	  	end
-	  	it "renders the show view" do
-	  		get :show, id: book.id
-	  		expect(response.status).to eq 200
-	  	end
-	  end
-
-    describe "POST 'create'" do
-    	let(:params) do
-    		{
-    			book: 
-    			{
-    				name: "this is a name"
-    			}
-    		}
-    	end
-
-    	let(:bad_params) do
-    		{
-    			book:
-    			{
-    				description: 'this'
-    			}
-    		}
-    	end
-
-    	# item created properly
-    	it "redirects to show page" do
-    		post :create, params
-    		expect(subject).to redirect_to book_path(1)
-    	end
-
-    	# error in creation
-    	it "renders new template" do
-    		post :create, bad_params
-    		expect(subject).to render_template("new")
-    	end
-    end
-
-   	describe "PATCH 'update'" do
-  		let(:book_id) { book.id }
-  		let(:update_params) do 
-  			{
-  					name: "Some book",
-  					description: "It's got music that I don't like :("
-  			}
-  		end
-  		it "should should be successful" do
-  			patch :update, { id: book_id, book: update_params }
-  			expect(subject).to redirect_to book_path(book_id)
-  		end
-
-  		it "should reload the page if not validated" do
-  			patch :update, { id: book_id, book: {name: nil}}
-  			expect(subject).to redirect_to edit_book_path(book_id)
-  		end
-  	end
-
-  	describe "PATCH 'upvote'" do
-  		before(:each) do
-  		  request.env["HTTP_REFERER"] = 'back'
-  		end
-
-  		it "should redirect to same page" do
-  			patch :upvote, id: book.id
-  			expect(subject).to redirect_to 'back'
-  		end
-  	end
-
-  	describe "DELETE 'destroy'" do
-  		it "should redirect to books page" do
-			  delete :destroy, id: book.id
-	  		expect(subject).to redirect_to books_path
-  		end
-  	end
-
-	end
+	it_behaves_like "a media controller"
 end
