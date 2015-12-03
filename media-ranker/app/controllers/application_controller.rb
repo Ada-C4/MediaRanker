043f0f20160702d41c82680d5460fc9b23b1cddb
Lauren Granger <lr.granger21@gmail.com>
2015-12-03 14:25:25 -0800

    application index pages has all body content and links

--- media-ranker/app/controllers/application_controller.rb
+++ media-ranker/app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
+    @books = Book.order(rank: :desc).limit(3)
+    @albums = Album.order(rank: :desc).limit(3)
+    @movies = Movie.order(rank: :desc).limit(3)
   end
 
 end
