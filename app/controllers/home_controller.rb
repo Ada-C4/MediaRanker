bdaefc402b5473f57373fd3086a7befa9015fa4f
emgord <emgord@gmail.com>
2015-12-03 10:30:51 -0800

    ranked and grabbed top ten for home page, added general medium spec and controller spec to begin drying up tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 
   def index
-    @movies = Movie.all
-    @albums = Album.all
-    @books = Book.all
+    @movies = Movie.all.sort_by { |movie| -movie.rank }.first(10)
+    @albums = Album.all.sort_by { |album| -album.rank }.first(10)
+    @books = Book.all.sort_by { |book| -book.rank }.first(10)
   end
 end
