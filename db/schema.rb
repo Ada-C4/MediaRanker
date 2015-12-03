1250f27e65991d2697cefe5b735be2eaf04119f2
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-03 14:19:58 -0800

    made new branch and destroyed models and controllers, eep

--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130213653) do
-
-  create_table "albums", force: :cascade do |t|
-    t.string   "name"
-    t.string   "artist"
-    t.string   "description"
-    t.integer  "upvotes"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "books", force: :cascade do |t|
-    t.string   "name"
-    t.string   "author"
-    t.string   "description"
-    t.integer  "upvotes"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "movies", force: :cascade do |t|
-    t.string   "name"
-    t.string   "director"
-    t.string   "description"
-    t.integer  "upvotes"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
