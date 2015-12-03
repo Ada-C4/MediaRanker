c0552377c079ad0785d4b84058dc5ef4ac3d9daa
Claire <ClaireS29@gmail.com>
2015-12-03 15:01:53 -0800

    Finished rspec

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -1,110 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe AlbumsController, type: :controller do
-  let(:create_params) do
-    {
-      album: {
-        name: "Peter Pan",
-        artist: "J.M. Barrie",
-        rank: 0
+  it_behaves_like "a medium controller" do
+    let(:create_params) do
+      {
+        album: {
+          name: "Peter Pan",
+          artist: "J.M. Barrie",
+          rank: 0
+        }
       }
-    }
-  end
+    end
 
-  let(:bad_params) do
-    {
-      id: album.id,
-      album: {
-        name: ""
+    let(:bad_params) do
+      {
+        id: medium.id,
+        album: {
+          name: ""
+        }
       }
-    }
-  end
+    end
 
-  let(:update_params) do
-    {
-      id: album.id,
-      album: {
-        name: "Peter",
-        artist: "Wendy",
-        rank: 0
+    let(:update_params) do
+      {
+        id: medium.id,
+        album: {
+          name: "Peter",
+          artist: "Wendy",
+          rank: 0
+        }
       }
-    }
 
-  end
-
-  let(:album) do
-    Album.create(create_params[:album])
-  end
-
-  describe "GET 'index'" do
-    it "is successful" do
-      get :index
-      expect(response.status).to eq 200
-    end
-  end
-
-  describe "GET 'show'" do
-    it "renders the show view for a move" do
-      get :show, id: album.id
-      expect(subject).to render_template :show
-    end
-  end
-
-  describe "POST 'create'" do
-       it "redirects to show page" do
-       post :create, create_params
-       a = Album.last
-       expect(subject).to redirect_to album_path(a.id)
-     end
-
-     it "renders new template on create error" do
-       post :create, bad_params
-       expect(subject).to render_template :new
-     end
-  end
-
-  describe "GET 'edit'" do
-
-    it "renders edit view" do
-      get :edit, id: album.id
-      expect(response.status).to eq 200
-    end
-  end
-
-  describe "DELETE 'destroy'" do
-    it "redirects to index view" do
-    delete :destroy, id: album.id
-    expect(subject).to redirect_to albums_path
-    end
-  end
-
-  describe "PATCH 'update'" do
-
-    it "successful update renders show view" do
-        patch :update, update_params
-        expect(response.status).to eq 302
-        expect(subject).to redirect_to album_path(album.id)
-      end
-
-      it "unsuccessful update renders edit form" do
-        patch :update, bad_params
-        expect(subject).to render_template :edit
-      end
     end
 
-  describe "PATCH 'upvote'" do
-
-    it "renders show view" do
-      patch :upvote, id: album.id
-      album.reload
-      expect(response.status).to eq 302
-      expect(subject).to redirect_to album_path(album.id)
+    let(:medium) do
+      Album.create(create_params[:album])
     end
 
-    it "increments votes" do
-      patch :upvote, id: album.id
-      album.reload
-      expect(album.rank).to eq 1
+    let(:show_1) do
+      album_path(1)
     end
   end
 end
