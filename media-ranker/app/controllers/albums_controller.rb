c7a56412eb5a736fec8e666f24fd9e083c188e47
Lauren Granger <lr.granger21@gmail.com>
2015-12-03 12:08:46 -0800

    methods for albums controller created

--- media-ranker/app/controllers/albums_controller.rb
+++ media-ranker/app/controllers/albums_controller.rb
@@ -1,2 +1,27 @@
 class AlbumsController < ApplicationController
+
+  def index
+  end
+
+  def create
+    redirect_to movies_path
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    redirect_to movies_path
+  end
+
+  def destroy
+    redirect_to movies_path
+  end
+
 end
