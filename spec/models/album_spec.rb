c8be6166e98a87bd62dc2be8a1df05606fcaa2cb
Ricky Hougland <houglande@gmail.com>
2015-12-03 10:16:32 -0800

    shared examples for models

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  describe ".validates" do
-    it "must have a name" do
-      album = Album.new(name: nil)
-      expect(album).to_not be_valid
-      expect(album.errors.keys).to include :name
-    end
-  end
-
+  it_behaves_like "a medium"
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  describe ".validates" do
-    it "must have a name" do
-      movie = Movie.new(name: nil)
-      expect(movie).to_not be_valid
-      expect(movie.errors.keys).to include :name
-    end
-  end
-
+  it_behaves_like "a medium"
 end
