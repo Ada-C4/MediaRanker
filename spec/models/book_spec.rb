ff7ba1e08d0d51e773db58ea3755841b167e8d8e
Meighan Rasley <meighanr@gmail.com>
2015-12-04 09:22:13 -0800

    Update book_spec to work with shared_examples

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,9 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe Book, type: :model do
-  describe ".validates" do
-    it "must have a name" do
-      expect(Book.new(name: nil)).to_not be_valid
-    end
-  end
+  it_behaves_like "a medium"
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,25 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe Movie, type: :model do
   it_behaves_like "a medium"
-end
-
-RSpec.describe Movie, type: :model do
-  describe ".validates" do
-    it "must have a name" do
-      expect(Movie.new(name: nil)).to_not be_valid
-    end
-    #
-    # it "must have a director" do
-    #   expect(Movie.new(director: nil)).to be_invalid
-    # end
-    #
-    # it "must have a description" do
-    #   expect(Movie.new(description: nil)).to be_invalid
-    # end
-    #
-    # it "must have a rank" do
-    #   expect(Movie.new(rank: nil)).to_not be_valid
-    # end
-  end
 end
