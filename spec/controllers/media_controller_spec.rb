cb58340aacb251f670f8b4fa1261c348cac54d33
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-04 14:58:35 -0800

    added specs for index, new, and show

--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -4,22 +4,26 @@ RSpec.describe MediaController, type: :controller do
 
   describe "GET #index" do
     it "renders the index view" do
-      get :index
+      get :index, type: "Movie"
       expect(subject).to render_template :index
-    end
-  end
-
-  describe "GET #show" do
-    it "renders the show view" do
-      get :show
-      expect(subject).to render_template :show
     end
   end
 
   describe "GET #new" do
     it "renders the new view" do
-      get :new
+      get :new, type: "Movie"
       expect(subject).to render_template :new
+    end
+  end
+
+  describe "GET #show" do
+    let(:movie) do
+      Medium.create(name: "Test", type: "Movie")
+    end
+
+    it "renders the show view" do
+      get :show, type: "Movie", id: movie.id
+      expect(subject).to render_template :show
     end
   end
 
