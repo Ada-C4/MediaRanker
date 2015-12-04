167db584b8d5506133e9bb55c409eb14230be1e0
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-12-04 15:45:16 -0800

    made post rspec work for create

--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -27,4 +27,32 @@ RSpec.describe MediaController, type: :controller do
     end
   end
 
+
+  describe "POST 'create'" do
+    let(:params) do
+      {
+        movie: {
+          name: "Test",
+          type: "Movie"
+        }
+      }
+    end
+
+    # let(:bad_params) do
+    #   {
+    #     medium: {}
+    #   }
+    # end
+
+    it "redirects to the show view" do
+      post :create, params.merge(type: "Movie")
+      expect(subject).to redirect_to polymorphic_path(Movie.all.last)
+    end
+
+    # it "renders the new view if there is an error" do
+    #   post :create, bad_params, type: "Movie"
+    #   expect(subject).to render_template :new
+    # end
+  end
+
 end
