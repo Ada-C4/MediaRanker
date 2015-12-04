e7a4d6269e90f197329ca7492225dbce220ffb62
Meighan Rasley <meighanr@gmail.com>
2015-12-04 14:12:24 -0800

    Attempt at media_controller, no success, revert back to previous state

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,10 +1,6 @@
-require 'rails_helper'
-
 RSpec.describe MoviesController, type: :controller do
-  # VERB 'action'
   describe "GET 'index'" do
     it "is successful" do
-      # verb :action
       get :index
       expect(response.status).to eq 200
     end
