f19f20a52cf45903b24e58df3eaf2fd05dbd4d4f
Meighan Rasley <meighanr@gmail.com>
2015-12-04 09:22:41 -0800

    Update album_spec to work with shared_examples

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,9 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe Album, type: :model do
-  describe ".validates" do
-    it "must have a name" do
-      expect(Album.new(name: nil)).to_not be_valid
-    end
-  end
+  it_behaves_like "a medium"
 end
